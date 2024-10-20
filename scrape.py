2fb37fb2d588e45f686c4f6e2a8c39952948ed37
Harinee Manikandan <harineemanikandan@gmail.com>
2024-10-20 22:53:57 +0530

    Removed

--- scrape.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-def fetch_webpage_content(url):
-    response=requests.get(url)
-    soup=BeautifulSoup(response.text,'html.parser')
-    paragraphs=soup.find_all('p')
-    content='\n'.join([para.get_text() for para in paragraphs])
-    return content
